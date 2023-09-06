330a6779528d79bf837b489f836d74a54eb35a19
Featthima Bou <featthimabou@Featthimas-MacBook-Pro.local>
2023-09-06 10:18:47 +0700

    task0 completed

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Featthima Bou", "sid": "6422790137"},
+    {"name": "Jaya Shree Hada", "sid": "6422790152"},
 ]
